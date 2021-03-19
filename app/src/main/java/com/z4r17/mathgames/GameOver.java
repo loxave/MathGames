79497e202b4971e2c952ce220a5576fa94d11a21
zenpardosi <zenfany54@gmail.com>
2021-03-19 16:04:18 +0700

    Initial commit

--- app/src/main/java/com/z4r17/mathgames/GameOver.java
+++ app/src/main/java/com/z4r17/mathgames/GameOver.java
@@ -1,8 +1,11 @@
 package com.z4r17.mathgames;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -11,6 +14,9 @@ import androidx.appcompat.app.AppCompatActivity;
 public class GameOver extends AppCompatActivity {
 
     TextView tvPoints;
+    SharedPreferences sharedPreferences;
+    ImageView ivHighScore;
+    TextView tvHighScore;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -18,8 +24,20 @@ public class GameOver extends AppCompatActivity {
         setContentView(R.layout.game_over);
         getSupportActionBar().hide();
         int points = getIntent().getExtras().getInt("points");
+        tvHighScore = findViewById(R.id.tvHighScore);
+        ivHighScore = findViewById(R.id.ivHighScore);
         tvPoints = findViewById(R.id.tvPoints);
+        sharedPreferences = getSharedPreferences("pref",0);
+        int pointsSP = sharedPreferences.getInt("pointsSP",0);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        if (points > pointsSP){
+            points =  points;
+            editor.putInt("pointsSP",pointsSP);
+            editor.commit();
+
+        }
         tvPoints.setText("" + points);
+        tvHighScore.setText(""  + pointsSP);
     }
 
     public void restart(View view) {
